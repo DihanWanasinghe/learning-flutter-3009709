2189d6b2bf812148e0c57b1e943b6af70277081b
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-01-05 12:01:39 +0530

    Fixed Errors

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ChatApp());
+  runApp(const MyApp());
 }
 
-class ChatApp extends StatelessWidget {
+// TODO Create ChatApp widget
+// TODO Create LoginPage widget
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -16,23 +22,48 @@ class ChatApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-// TODO: Move LoginPage code to it's own file
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      //TODO: Add the text and image from the design
-      body: Text('Hello!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
