207b39386867796ce9b5c607d645304a588372ea
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-01-21 13:35:40 +0530

    Button Exercise done

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+
+void buttonCLicked(){
+
+  print("Clicked Button");
+}
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -7,36 +13,49 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
 
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
+              ElevatedButton(onPressed:
+                buttonCLicked
+              ,
+                  child: Text('Click Me!',
+                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),
+                  )
+              ),
+              TextButton(onPressed: () {
+                print("link clicked ");
+              }, child: Text('DihanWanasinghe/github.com'))
+              //TODO: Add Login Button
 
-            //TODO: Add Login Button
-
-            //TODO: Add URL Text Button
-          ],
+              //TODO: Add URL Text Button
+            ],
+          ),
         ),
       ),
     );
