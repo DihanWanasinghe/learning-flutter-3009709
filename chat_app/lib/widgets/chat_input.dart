189db7beb5235ecf9360500a3f4423f46eae6f2f
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-01-26 04:27:31 +0530

    TextInput done

--- chat_app/lib/widgets/chat_input.dart
+++ chat_app/lib/widgets/chat_input.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
-  const ChatInput({Key? key}) : super(key: key);
+   ChatInput({Key? key}) : super(key: key);
+  final chatMessageController = TextEditingController();
+
+  void sendChat(){
+
+    print('ChatMessage:${chatMessageController.text}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +23,32 @@ class ChatInput extends StatelessWidget {
               color: Colors.white,
             ),
           ),
+          Expanded(
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 2,
+                minLines: 1,
+                style: TextStyle(color: Colors.white,),
+                controller: chatMessageController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Type your Message",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+
+
+
+
+                ),
+
+              )
+          ),
           //TODO: Add multi-line TextField here
           IconButton(
-            onPressed: () {},
+            onPressed: sendChat,
             icon: Icon(
               Icons.send,
               color: Colors.white,
+
             ),
           ),
         ],
