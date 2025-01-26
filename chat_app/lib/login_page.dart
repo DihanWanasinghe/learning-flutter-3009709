05df84f4f3e4c72b18dbdf577e5124a8ed9ea66f
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-01-26 10:11:25 +0530

    Navigation from Login Page to Chat Page  - done

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/chat_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,10 +6,11 @@ class LoginPage extends StatelessWidget {
 
   final _formkey = GlobalKey<FormState>();
 
-  void loginUser() {
+  void loginUser(context) {
     if (_formkey.currentState != null && _formkey.currentState!.validate()) {
       print(userNameController.text);
       print(passwordController.text);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
 
       //TODO: Navigate to ChatPage on successful login
       print('login successful!');
@@ -93,7 +95,9 @@ class LoginPage extends StatelessWidget {
                 height: 24,
               ),
               ElevatedButton(
-                  onPressed: loginUser,
+                  onPressed: (){
+                    loginUser(context);
+                  },
                   child: Text(
                     'Login',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
